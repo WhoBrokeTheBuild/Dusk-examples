e4084749eb8837f268a9503a4ebebc265cf4a020
Stephen Lane-Walsh <sdl.slane@gmail.com>
2017-05-28 01:24:52 -0400

    Updates, added EventData to lua

--- Cube/assets/scripts/Cube-example.lua
+++ Cube/assets/scripts/Cube-example.lua
@@ -3,9 +3,9 @@ local app = Dusk.GetApp()
 local scene = app:GetScene()
 local cube = scene:GetActorByName("cube_actor")
 
-function cube_Update()
+function cube_Update(data)
     local x, y, z = cube:GetRotation()
-    y = y + 0.1
+    y = y + (50.0 * data.Delta)
     cube:SetRotation(x, y, z)
 end
 
