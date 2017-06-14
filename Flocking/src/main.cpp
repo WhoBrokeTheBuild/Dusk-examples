c6a43518f31b0d894ce28b34b8b4a6c7c155b155
Stephen Lane-Walsh <sdl.slane@gmail.com>
2017-06-14 11:37:50 -0400

    Cleanup and improvements on the Flocking example

--- Flocking/src/main.cpp
+++ Flocking/src/main.cpp
@@ -2,9 +2,21 @@
 #include <sstream>
 #include <random>
 
-const unsigned int BOID_COUNT = 100;
+const unsigned int BOID_COUNT = 500;
+const float SPEED = 1000.0f;
+
 const glm::vec3 UPPER_BOUND = glm::vec3(300.0f);
 const glm::vec3 LOWER_BOUND = glm::vec3(-300.0f);
+
+const double NEIGHBORS_MAX_TIMEOUT = 2.0;
+
+const double COHESION_RADIUS   = 80;
+const double SEPARATION_RADIUS = 50;
+const double ALIGNMENT_RADIUS  = 80;
+
+const float COHESION_WEIGHT   = 0.5;
+const float SEPARATION_WEIGHT = 0.5;
+const float ALIGNMENT_WEIGHT  = 0.6;
 
 std::random_device _R;
 
@@ -39,15 +51,8 @@ public:
 
 private:
 
-    const double NEIGHBORS_MAX_TIMEOUT = 0.3;
-    const double COHESION_RADIUS = 50;
-    const double SEPARATION_RADIUS = 25;
-    const double ALIGNMENT_RADIUS = 50;
-    const float COHESION_WEIGHT = 1.0;
-    const float SEPARATION_WEIGHT = 1.5;
-    const float ALIGNMENT_WEIGHT = 1.0;
-
     double _neighborsTimeout = 0;
+
     std::vector<BoidActor *> _cohesionNeighbors;
     std::vector<BoidActor *> _separationNeighbors;
     std::vector<BoidActor *> _alignmentNeighbors;
@@ -66,17 +71,22 @@ public:
         dusk::App * app = dusk::App::GetInst();
         dusk::Shader * shader = app->GetShader("default_shader");
         dusk::Mesh * mesh = new dusk::ConeMesh(shader, nullptr, 5, 3.0f, 8.0f);
-        mesh->SetRotation(glm::vec3(0, 0, M_PI * 0.5f));
+        mesh->SetRotation(glm::vec3(0, 0, -glm::pi<float>() * 0.5f));
 
         AddComponent(new dusk::MeshComponent(this, mesh));
         AddComponent(new FlockingComponenet(this));
 
         std::default_random_engine reng(_R());
-        std::uniform_real_distribution<float> uniform_dist(-1, 1);
-        glm::vec3 vel = glm::vec3(uniform_dist(reng),
-                                  uniform_dist(reng),
-                                  uniform_dist(reng));
+        std::uniform_real_distribution<float> random_position(LOWER_BOUND.x, UPPER_BOUND.x);
+        std::uniform_real_distribution<float> random_angle(-1, 1);
 
+        SetPosition(glm::vec3(random_position(reng),
+                              random_position(reng),
+                              random_position(reng)));
+
+        glm::vec3 vel = glm::vec3(random_angle(reng),
+                                  random_angle(reng),
+                                  random_angle(reng));
         SetVelocity(vel * SPEED);
     }
 
@@ -121,8 +131,6 @@ public:
     void SetAcceleration(const glm::vec3& acc) { _acceleration = acc; }
     glm::vec3 GetAcceleration() const { return _acceleration; }
 
-    const float SPEED = 1000.0f;
-
 private:
 
     glm::vec3 _velocity;
@@ -141,7 +149,7 @@ void FlockingComponenet::Update(const dusk::Event& event)
     if (_neighborsTimeout <= 0)
     {
         std::default_random_engine reng(_R());
-        std::uniform_real_distribution<float> uniform_dist(NEIGHBORS_MAX_TIMEOUT / 2, NEIGHBORS_MAX_TIMEOUT);
+        std::uniform_real_distribution<double> uniform_dist(NEIGHBORS_MAX_TIMEOUT / 2, NEIGHBORS_MAX_TIMEOUT);
         _neighborsTimeout = uniform_dist(reng);
 
         glm::vec3 pos = GetActor()->GetPosition();
@@ -150,6 +158,10 @@ void FlockingComponenet::Update(const dusk::Event& event)
         _separationNeighbors.clear();
         _alignmentNeighbors.clear();
 
+        float cohesionSquared   = COHESION_RADIUS * COHESION_RADIUS;
+        float separationSquared = SEPARATION_RADIUS * SEPARATION_RADIUS;
+        float alignmentSquared  = ALIGNMENT_RADIUS * ALIGNMENT_RADIUS;
+
         std::vector<dusk::Actor *> allBoids = GetActor()->GetScene()->GetActorsByTag("Boid");
         for (dusk::Actor * actor : allBoids)
         {
@@ -157,16 +169,16 @@ void FlockingComponenet::Update(const dusk::Event& event)
 
             if (actor == GetActor()) continue;
 
-            float dist = glm::distance(actor->GetPosition(), pos);
-            if (dist < COHESION_RADIUS)
+            float dist = glm::distance2(actor->GetPosition(), pos);
+            if (dist < cohesionSquared)
             {
                 _cohesionNeighbors.push_back(boid);
             }
-            if (dist < SEPARATION_RADIUS)
+            if (dist < separationSquared)
             {
                 _separationNeighbors.push_back(boid);
             }
-            if (dist < ALIGNMENT_RADIUS)
+            if (dist < alignmentSquared)
             {
                 _alignmentNeighbors.push_back(boid);
             }
@@ -178,58 +190,58 @@ void FlockingComponenet::Update(const dusk::Event& event)
     glm::vec3 velocity = boidActor->GetVelocity();
 
     glm::vec3 cohesionVel;
+    glm::vec3 separationVel;
+    glm::vec3 alignmentVel;
+
     if (!_cohesionNeighbors.empty())
     {
-        glm::vec3 cohesionDest;
+        glm::vec3 center;
+
         for (BoidActor * boid : _cohesionNeighbors)
+            center += boid->GetPosition();
+
+        center /= (float)_cohesionNeighbors.size();
+        cohesionVel = center - GetActor()->GetPosition();
+
+        if (glm::length2(cohesionVel) > 0)
         {
-            cohesionDest += boid->GetPosition();
+            cohesionVel = glm::normalize(cohesionVel) * SPEED;
         }
-        cohesionDest = cohesionDest / (float)(_cohesionNeighbors.size());
-        cohesionVel = glm::normalize(cohesionDest - GetActor()->GetPosition());
-        cohesionVel *= boidActor->SPEED;
-        cohesionVel -= velocity;
-        cohesionVel = glm::clamp(cohesionVel, glm::vec3(-boidActor->SPEED), glm::vec3(boidActor->SPEED));
-        //DuskLogInfo("%f %f %f", cohesionDest.x, cohesionDest.y, cohesionDest.z);
     }
 
-    glm::vec3 separationVel;
     if (!_separationNeighbors.empty())
     {
-        glm::vec3 separationDest;
-        for (BoidActor * boid : _separationNeighbors)
-        {
-            separationDest += boid->GetPosition();
-        }
-        separationDest = separationDest / (float)(_separationNeighbors.size());
-        separationVel = glm::normalize(separationDest - GetActor()->GetPosition());
+        glm::vec3 center;
 
-        if (glm::length(separationVel) > 0)
+        for (BoidActor * boid : _separationNeighbors)
+            center += boid->GetPosition();
+
+        center /= (float)_separationNeighbors.size();
+
+        separationVel = GetActor()->GetPosition() - center;
+
+        if (glm::length2(separationVel) > 0)
         {
-            separationVel *= boidActor->SPEED;
-            separationVel -= velocity;
-            separationVel *= -1.0f;
-            separationVel = glm::clamp(separationVel, glm::vec3(-boidActor->SPEED), glm::vec3(boidActor->SPEED));
+            separationVel = glm::normalize(separationVel) * SPEED;
         }
-        //DuskLogInfo("%f %f %f", separationDest.x, separationDest.y, separationDest.z);
     }
 
-    glm::vec3 alignmentVel;
     if (!_alignmentNeighbors.empty())
     {
         for (BoidActor * boid : _alignmentNeighbors)
-        {
             alignmentVel += boid->GetVelocity();
-        }
-        alignmentVel = glm::normalize(alignmentVel / (float)_alignmentNeighbors.size());
-        alignmentVel -= velocity;
-        alignmentVel = glm::clamp(alignmentVel, glm::vec3(-boidActor->SPEED), glm::vec3(boidActor->SPEED));
+
+        alignmentVel /= (float)_alignmentNeighbors.size();
     }
 
-    glm::vec3 acceleration = (cohesionVel * COHESION_WEIGHT) +
-                             (separationVel * SEPARATION_WEIGHT) +
-                             (alignmentVel * ALIGNMENT_WEIGHT);
-    boidActor->SetAcceleration(acceleration);
+    float totalWeight = COHESION_WEIGHT + SEPARATION_WEIGHT + ALIGNMENT_WEIGHT;
+
+    velocity += cohesionVel * (COHESION_WEIGHT / totalWeight);
+    velocity += separationVel * (SEPARATION_WEIGHT / totalWeight);
+    velocity += alignmentVel * (ALIGNMENT_WEIGHT / totalWeight);
+
+    velocity = glm::normalize(velocity) * SPEED;
+    boidActor->SetVelocity(velocity);
 }
 
 struct LightingData
@@ -261,8 +273,6 @@ void AppStart(const dusk::Event& event)
         ss.clear();
         ss.str("boid-");
         ss << i;
-
-        DuskLogInfo("Spawning boid #%d", i + 1);
 
         dusk::Actor * actor = (dusk::Actor *)new BoidActor(scene, ss.str());
         app->GetScene()->AddActor(actor);
