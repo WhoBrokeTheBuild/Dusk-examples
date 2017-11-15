09ddb41a22ab40ff0cc368d542aa78ab6e359945
Stephen Lane-Walsh <sdl.slane@gmail.com>
2017-11-15 12:06:13 -0800

    Updated to work with new Actor

--- Textured/assets/shaders/textured.vs.glsl
+++ Textured/assets/shaders/textured.vs.glsl
@@ -5,13 +5,6 @@ in vec3 _Position;
 in vec3 _Normal;
 in vec2 _TexCoord;
 
-layout(std140) uniform LightingData
-{
-    vec3 LightPos;
-    vec3 CameraPos;
-
-} _LightingData;
-
 out vec4 p_Position;
 out vec4 p_Normal;
 out vec2 p_TexCoord;
@@ -25,10 +18,10 @@ void main()
     p_Normal   = _TransformData.Model * vec4(_Normal, 1.0);
     p_TexCoord = _TexCoord;
 
-    p_LightDir = _LightingData.LightPos - p_Position.xyz;
+    p_LightDir = vec3(3, 3, 3) - p_Position.xyz;
     p_LightDir = normalize(p_LightDir);
 
-    p_ViewDir = _LightingData.CameraPos - p_Position.xyz;
+    p_ViewDir = vec3(3, 3, 3) - p_Position.xyz;
     p_ViewDir = normalize(p_ViewDir);
 
     gl_Position = _TransformData.MVP * vec4(_Position, 1.0);
--- Textured/src/Textured.cpp
+++ Textured/src/Textured.cpp
@@ -1,66 +1,45 @@
 #include <dusk/Main.hpp>
 #include <dusk/Dusk.hpp>
-#include "TexturedShader.hpp"
 #include <sstream>
 
+dusk::Shader * shader;
 dusk::Camera * camera;
 dusk::Model * model;
-TexturedShader * sp;
 
 void Update(const dusk::UpdateContext& ctx)
 {
     static float rotation = 0.0f;
     rotation += 0.5f * ctx.DeltaTime;
     rotation = fmod(rotation, 360.0f);
-
     glm::vec3 rot = model->GetRotation();
     rot.y = glm::radians(rotation);
-    model->SetRotation(rot);
 
-    model->Update(ctx);
+    model->SetRotation(rot);
 }
 
 void Render(dusk::RenderContext& ctx)
 {
-    ctx.CurrentShader = sp;
+    ctx.CurrentShader = shader;
     ctx.CurrentCamera = camera;
-    model->Render(ctx);
 }
 
 void _DuskSetup(dusk::App * app)
 {
-    dusk::Scene * scene = app->AddScene(dusk::Scene::Create());
-
-    dusk::Actor * actor = scene->AddActor(dusk::Actor::Create());
-    actor->SetPosition({ 0, 0, 0 });
-
-    dusk::ModelComponent * modelComp = actor->AddComponent<dusk::ModelComponent>(dusk::ModelComponent::Create(dusk::Model::Create()));
-    model = modelComp->GetModel();
-    model->AddMesh(dusk::Mesh::Create("assets/models/globe/globe.obj"));
-    //model->AddMesh(dusk::Mesh::Create("/home/stephen/Downloads/dota2/models/heroes/meepo/fbx/meepo_arms.fbx"));
-    //model->AddMesh(dusk::Mesh::Create("/home/stephen/Downloads/dota2/models/heroes/meepo/fbx/meepo_back.fbx"));
-    //model->AddMesh(dusk::Mesh::Create("/home/stephen/Downloads/dota2/models/heroes/meepo/fbx/meepo_body_tex.fbx"));
-    //model->AddMesh(dusk::Mesh::Create("/home/stephen/Downloads/dota2/models/heroes/meepo/fbx/meepo_head.fbx"));
-    //model->AddMesh(dusk::Mesh::Create("/home/stephen/Downloads/dota2/models/heroes/meepo/fbx/meepo_shoulders.fbx"));
-    //model->AddMesh(dusk::Mesh::Create("/home/stephen/Downloads/dota2/models/heroes/meepo/fbx/meepo_tail.fbx"));
-    //model->AddMesh(dusk::Mesh::Create("/home/stephen/Downloads/dota2/models/heroes/meepo/fbx/meepo_weapon.fbx"));
-
-    dusk::Box bounds = model->GetBounds();
-
-    camera = scene->AddCamera(dusk::Camera::Create());
-    camera->SetAspect(app->GetWindowSize());
-    camera->SetPosition(bounds.Max + (bounds.GetSize() * 0.5f));
-    camera->SetForward(bounds.Min - bounds.Max);
-
-    app->EvtWindowResize.AddStatic([&](glm::ivec2 size) {
-        camera->SetAspect(size);
-    });
-
-    sp = static_cast<TexturedShader *>(app->AddShader(std::make_unique<TexturedShader>()));
-    sp->SetLightPos(camera->GetPosition());
-    sp->SetCameraPos(camera->GetPosition());
+    shader = app->AddShader(std::unique_ptr<dusk::Shader>(new dusk::Shader({ "assets/shaders/textured.fs.glsl", "assets/shaders/textured.vs.glsl" })));
 
     app->EvtUpdate.AddStatic(&Update);
     app->EvtRender.AddStatic(&Render);
 
+    dusk::Scene * scene = app->AddScene("main", std::make_unique<dusk::Scene>());
+    app->SetActiveScene("main");
+
+    camera = (dusk::Camera *)scene->AddChild(std::make_unique<dusk::Camera>(), "camera_main");
+    camera->SetAspect(app->GetWindowSize());
+    camera->SetPosition({ 3, 3, 3 });
+    camera->SetForward({ -1, -1, -1 });
+
+    dusk::Actor * tmp = scene->AddChild(std::make_unique<dusk::Actor>(), "actor_main");
+
+    model = tmp->AddChild<dusk::Model>(std::make_unique<dusk::Model>(), "model_globe");
+    model->AddMesh(std::make_shared<dusk::Mesh>("assets/models/globe/globe.obj"));
 }
