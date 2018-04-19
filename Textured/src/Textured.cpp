ed67c9102c062f46d02a1cff55f839126a44a9d3
Stephen Lane-Walsh <sdl.slane@gmail.com>
2018-04-19 12:28:28 -0400

    Updated example to work with engine changes

--- Textured/src/Textured.cpp
+++ Textured/src/Textured.cpp
@@ -1,56 +1,7 @@
-#include <dusk/Main.hpp>
-#include <dusk/Dusk.hpp>
-#include <dusk/core/Event.hpp>
-#include <sstream>
-
-dusk::ScriptHost script;
-
-dusk::Shader * shader;
-dusk::Camera * camera;
-dusk::Model * model;
-
-std::unique_ptr<dusk::ICallback> updateCb, renderCb, reloadCb;
-
-void Update(const dusk::UpdateContext& ctx)
-{
-    static float rotation = 0.0f;
-    rotation += 0.5f * ctx.DeltaTime;
-    rotation = fmod(rotation, 360.0f);
-    glm::vec3 rot = model->GetRotation();
-    rot.y = glm::radians(rotation);
-
-    model->SetRotation(rot);
-}
-
-void Render(dusk::RenderContext& ctx)
-{
-    ctx.CurrentShader = shader;
-    ctx.CurrentCamera = camera;
-}
+#include "Textured.hpp"
 
 void _DuskSetup(dusk::App * app)
 {
-    script.LoadFile("assets/scripts/main.as");
-    script.Run();
-
-    shader = app->AddShader(std::unique_ptr<dusk::Shader>(new dusk::Shader({ "assets/shaders/textured.fs.glsl", "assets/shaders/textured.vs.glsl" })));
-
-    updateCb.reset();
-    updateCb = app->OnUpdate.AddStatic(&Update);
-
-    renderCb.reset();
-    renderCb = app->OnRender.AddStatic(&Render);
-
-    dusk::Scene * scene = app->AddScene("main", std::make_unique<dusk::Scene>());
-    app->SetActiveScene("main");
-
-    camera = (dusk::Camera *)scene->AddChild(std::make_unique<dusk::Camera>(), "camera_main");
-    camera->SetAspect(app->GetWindowSize());
-    camera->SetPosition({ 3, 3, 3 });
-    camera->SetForward({ -1, -1, -1 });
-
-    dusk::Actor * tmp = scene->AddChild(std::make_unique<dusk::Actor>(), "actor_main");
-
-    model = tmp->AddChild<dusk::Model>(std::make_unique<dusk::Model>(), "model_globe");
-    model->AddMesh(std::make_shared<dusk::Mesh>("assets/models/globe/globe.obj"));
+    app->AddScene("main_scene", std::make_unique<GameScene>());
+    app->SetActiveScene("main_scene");
 }
---
+++ Textured/src/Textured.hpp
@@ -0,0 +1,65 @@
+#ifndef TEXTURED_HPP
+#define TEXTURED_HPP
+
+#include <dusk/Main.hpp>
+#include <dusk/Dusk.hpp>
+#include <dusk/core/Event.hpp>
+
+#include <sstream>
+
+class GameScene : public dusk::Scene
+{
+public:
+
+    DUSK_CLASS(GameScene)
+
+    virtual void Start() override
+    {
+        Scene::Start();
+
+        script.LoadFile("assets/scripts/main.as");
+        script.Run();
+
+        dusk::App * app = dusk::App::Inst();
+
+        dusk::Shader * shader = app->AddShader(std::make_unique<dusk::Shader>(std::vector<std::string>({
+            "assets/shaders/textured.fs.glsl",
+            "assets/shaders/textured.vs.glsl"
+        })));
+
+        TrackCallback(app->OnUpdate.AddMember<GameScene>(this, &GameScene::Update));
+
+        dusk::Camera * camera = AddActor<dusk::Camera>(std::make_unique<dusk::Camera>(this));
+        camera->SetId("main_camera");
+        camera->SetAspect(app->GetWindowSize());
+        camera->SetPosition({ 3, 3, 3 });
+        camera->SetForward({ -1, -1, -1 });
+
+        actor = AddActor(std::make_unique<dusk::Actor>(this));
+        actor->SetId("main_actor");
+        actor->AddComponent(std::make_unique<dusk::MeshComponent>(actor, std::make_unique<dusk::Mesh>("assets/models/globe/globe.obj")));
+
+        app->GetRenderContext().CurrentShader = shader;
+        app->GetRenderContext().CurrentCamera = camera;
+    }
+
+    void Update(const dusk::UpdateContext& ctx)
+    {
+        static float rotation = 0.0f;
+        rotation += 0.5f * ctx.DeltaTime;
+        rotation = fmod(rotation, 360.0f);
+
+        glm::vec3 rot = actor->GetRotation();
+        rot.y = glm::radians(rotation);
+        actor->SetRotation(rot);
+    }
+
+private:
+
+    dusk::ScriptHost script;
+
+    dusk::Actor * actor;
+
+}; // class GameScene
+
+#endif // TEXTURED_HPP
